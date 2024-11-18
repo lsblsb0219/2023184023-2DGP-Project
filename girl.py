6ba1eed51d47ab51b118fa9d7dec1a9643c143a0
subinlee@notebook <lsbin0219@gmail.com>
2024-11-18 17:05:50 +0900

    위아래 이동 구현

--- girl.py
+++ girl.py
@@ -1,16 +1,22 @@
 from pico2d import load_image
+
+from mouse import handle_mouse_events
 from state_machine import *
+import mouse
 
 class Idle:
     @staticmethod
     def enter(girl, e):
-        if start_event(e):
+        if start_event(e) or up_down(e) or down_up(e):  # 뒤
             girl.action = 12
-            girl.xface_dir = 1
-        elif right_down(e) or left_up(e):
+            girl.yface_dir = -1
+        elif down_down(e) or up_up(e): # 앞
+            girl.action = 10
+            girl.yface_dir = 1
+        elif right_down(e) or left_up(e): # 왼쪽
             girl.action = 9
             girl.xface_dir = -1
-        elif left_down(e) or right_up(e):
+        elif left_down(e) or right_up(e): # 오른쪽
             girl.action = 11
             girl.xface_dir = 1
 
@@ -33,9 +39,13 @@ class Run:
     @staticmethod
     def enter(girl, e):
         if right_down(e) or left_up(e):  # 오른쪽으로 RUN
-            girl.dir, girl.xface_dir, girl.action = 1, 1, 11
+            girl.dir, girl.xface_dir, girl.yface_dir, girl.action = 1, 1, 0, 11
         elif left_down(e) or right_up(e):  # 왼쪽으로 RUN
-            girl.dir, girl.xface_dir, girl.action = -1, -1, 9
+            girl.dir, girl.xface_dir, girl.yface_dir, girl.action = -1, -1, 0, 9
+        elif down_down(e) or up_up(e): # 아래로 RUN
+            girl.dir, girl.xface_dir, girl.yface_dir, girl.action = -1, 0, 1, 12
+        elif up_down(e) or down_up(e): # 위로 RUN
+            girl.dir, girl.xface_dir, girl.yface_dir, girl.action = 1, 0, -1, 10
 
     @staticmethod
     def exit(girl, e):
@@ -45,13 +55,17 @@ class Run:
     @staticmethod
     def do(girl):
         girl.frame = (girl.frame + 1) % 4
-        if 0 < girl.x + girl.dir * 2 < 800:
-            girl.x += girl.dir * 2
+        if girl.yface_dir == 0:
+            if 0 < girl.x + girl.dir * 2 < 800:
+                girl.x += girl.dir * 2
+        elif girl.xface_dir == 0:
+            if 140 < girl.y + girl.dir * 2 < 400:
+                girl.y += girl.dir * 2
         pass
 
     @staticmethod
     def draw(girl):
-        girl.image.clip_draw(girl.frame * 16, girl.action * 32, 16, 32, girl.x, girl.y, 16 * 3, 32 * 3)
+            girl.image.clip_draw(girl.frame * 16, girl.action * 32, 16, 32, girl.x, girl.y, 16 * 3, 32 * 3)
 
 
 
@@ -64,13 +78,14 @@ class Girl:
         self.state_machine.start(Idle)
         self.state_machine.set_transitions(
             {
-                Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run},
-                Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+                Idle: {down_down: Run, up_down: Run, down_up: Run, up_up: Run, right_down: Run, left_down: Run, left_up: Run, right_up: Run},
+                Run: {down_down: Idle, up_down: Idle, down_up: Idle, up_up: Idle, right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
             }
         )
 
     def update(self):
         self.state_machine.update()
+        # handle_mouse_events()
 
     def handle_event(self, event):
         # 여기서 받을 수 있는 것만 걸러야 함. right left  등등..
